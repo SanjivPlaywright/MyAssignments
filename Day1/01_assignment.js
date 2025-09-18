2f851570225c4a45f69bd8c1d3e97b647cc43406
SanjivPlaywright <s.sanjiv.kumar@gmail.com>
2025-09-18 18:19:07 +0530

    Add files via upload

---
+++ Day1/01_assignment.js
@@ -0,0 +1,11 @@
+var firstname='Sanjiv Kumar';
+var companyname="Accionlabs";
+var mobileNumber=9791244415;
+
+console.log("Firstname: "+ firstname); 
+console.log("Company Name: "+companyname);
+console.log("Mobile number: "+mobileNumber);
+
+console.log("Datatype of firstname is "+typeof firstname);
+console.log("Datatype of Company Name is "+typeof companyname);
+console.log("Datatype of MobileNumber is "+typeof mobileNumber);
